c5cfcde0f197915d3e3b5db4d64b7bbe8e919835
Winai Nadee <winain@gmail.com>
2024-03-11 16:17:53 +0700

    Frontend Framework - Svelte

---
+++ week08/src/lib/Quantifier.svelte
@@ -0,0 +1,17 @@
+<script>
+	let quantity = 0
+
+	function increment() {
+		quantity += 1
+	}
+	function decrement() {
+		if (quantity === 0) {
+			return
+		}
+		quantity -= 1
+	}
+</script>
+
+<span>Current quantity: {quantity}</span>
+<button on:click={decrement}>-</button>
+<button on:click={increment}>+</button>
---
+++ week08/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Quantifier from "$lib/Quantifier.svelte";
+</script>
+
+<div><Quantifier /></div>
+<div><Quantifier /></div>
+<div><Quantifier /></div>
+<div><Quantifier /></div>
+<div><Quantifier /></div>
+<div><Quantifier /></div>
